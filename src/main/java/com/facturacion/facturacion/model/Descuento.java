43445951e87275f3d6b648c42be10d9936d3ea04
Felipe-Ahumada <felipe.ahumada.silva2001@gmail.com>
2025-05-27 13:50:02 -0400

    minor bug fixes

--- src/main/java/com/facturacion/facturacion/model/Descuento.java
+++ src/main/java/com/facturacion/facturacion/model/Descuento.java
@@ -25,7 +25,7 @@ public class Descuento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nombre;
 
     @Column(nullable = false)
